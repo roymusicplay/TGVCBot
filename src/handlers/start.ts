1590ac03d5e08b8da030052e31559332b6a3b07c
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-08-21 17:47:43 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -13,10 +13,10 @@ export const Start = Composer.command('start', async (ctx) => {
     let text =
         `Hi <a href="tg://user?id=${ctx.from.id}">${escape(ctx.from.first_name + " " + ctx.from.last_name)}</a>\n` +
         `I Play Songs in Group Voice Chats.\n` +
-        `If you like this bot consider adding a star to the <a href="https://github.com/ArnabXD/TGVCBot">repository</a>`
+        `If you like this bot consider adding a star to the <a href="https://t.me/abhinasroy">repository</a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
-            [Markup.button.url('View Source', 'https://github.com/ArnabXD/TGVCBot')]
+            [Markup.button.url('View Source', 'https://t.me/abhinasroy')]
         ]),
         disable_web_page_preview: true
     })
